62995ab2996599cfcc3cf799d263cabd6ccb8455
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-13 23:44:46 +0530

    with fb app integration

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
-/*
+
 FB.api('/me', function(response) {
-    app.post("Hello"+JSON.stringify(response.name));
+    res.send('Hello ' + response.name + '! How are you?');
 });
-*/

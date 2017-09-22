b7069ab32c5c45485c4dbb5fb8b64a4f1c33fa8b
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-22 21:30:23 +0530

    Final app with mongodb hookup

--- server/_config.js
+++ server/_config.js
@@ -2,7 +2,7 @@ var ids = {
   facebook: {
     clientID: '1440315662672851',
     clientSecret: 'c63fe066d97d58fc7c378c5be0411f2f',
-    callbackURL: 'http://ec2-52-15-226-69.us-east-2.compute.amazonaws.com/auth/facebook/callback'
+    callbackURL: 'ec2-18-221-155-123.us-east-2.compute.amazonaws.com/auth/facebook/callback'
   },
     database: {
       username:'rahul',

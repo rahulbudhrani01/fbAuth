b1b7ca42eea298e1169a7369ef5bbd26b0b2eb0d
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-23 19:24:45 +0530

    Saving user details in db

--- server/app.js
+++ server/app.js
@@ -59,5 +59,25 @@ app.use(passport.session());
 app.use('/', routes);
 
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// *** error handlers *** //
+
+
+// production error handler
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    
+  });
+});
 
 app.listen(3000);

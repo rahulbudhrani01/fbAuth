c3954b5c6f9878dbee574ffce57bbd3b3a946c4c
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-23 21:27:43 +0530

    User details in db

--- server/auth/init.js
+++ server/auth/init.js
@@ -6,12 +6,12 @@ module.exports = function() {
 
   passport.serializeUser(function(user, done) {
     console.log("Serializing");
-    done(null, user.id.toString());
+    done(null, user.id);
   });
 
   passport.deserializeUser(function(id, done) {
     console.log("Derializing");
-    User.findById(id.toString(), function (err, user) {
+    User.findById(id, function (err, user) {
       done(err, user);
     });
   });

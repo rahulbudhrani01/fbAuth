79870bb40bb7a11570921f8004c4d4b06236125b
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-24 15:32:06 +0530

    User details in db

--- server/auth/facebook.js
+++ server/auth/facebook.js
@@ -35,17 +35,23 @@ passport.use(new FacebookStrategy({
 
 
     // update the user if s/he exists or add a new user
-    User.findOneAndUpdate({name:"Rahul Budhrani"}, {$set:{someID: "10210602909377094"}}/*, {new: true}*/, function(err, user) {
-        console.log("Inside findOneAndUpdate");
-      if(err) {
-          throw err;
-        return done(err);
-      } else {
-        console.log("Updated");
-        return done(null, profile);
+    User.findOne({ 'someID': profile.id }, function(err, user) {
+       if (err)
+         return done(err);
+       if (user) {
+         return done(null, user);
+       } else {
+         var newUser = new User();
+         newUser.someID = profile.id;
+         //newUser.facebook.token = token;
+         newUser.name = profile.displayName;
+         //newUser.facebook.email = (profile.emails[0].value || '').toLowerCase();
 
-      }
-    });
+         newUser.save(function(err) {
+           if (err)
+             throw err;
+           return done(null, newUser);
+         });
   }));
 
 // serialize user into the session

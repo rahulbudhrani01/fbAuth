a87ffa142693ac2b9849f5a618a994cc4849fe2b
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-26 01:58:31 +0530

    User details in db

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,7 +20,7 @@ router.get('/login/facebook', passportFacebook.authenticate('facebook'));
 
 router.get('/login/facebook/return',
   passportFacebook.authenticate('facebook', { failureRedirect: '/login' }),  function(req, res) {
-    res.redirect('/');
+    res.redirect('/profile');
 
     //Successful authentication
   // res.json(req.user);
@@ -29,7 +29,7 @@ router.get('/login/facebook/return',
   });
 
 router.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
+  //require('connect-ensure-login').ensureLoggedIn(),
   function(req, res){
     res.render('profile', { user: req.user });
   });

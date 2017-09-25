036b0dd5d898123d05c3080dd95af95ec8405ba3
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-25 11:37:39 +0530

    User details in db

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ var app = express();
 
 
 // *** mongoose *** //
-mongoose.connect('mongodb://config.database.username:config.database.password@18.221.155.123:27017/passport-social-auth',  { useMongoClient: true },{ promiseLibrary: require('bluebird') });
+mongoose.connect('mongodb://config.database.username:config.database.password@18.221.155.123:27017/passport-social-auth',  { useMongoClient: true, promiseLibrary: require('bluebird') });
 //mongoose.connect('mongodb://127.0.0.1:27017/passport-social-auth');
 
 

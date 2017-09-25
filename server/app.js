96885f6d2267f7ce48457bca4785c6a5c114b6b2
RAHUL BUDHRANI <rahulbudhrani@RAHULs-MacBook-Pro.local>
2017-09-25 12:50:24 +0530

    User details in db

--- server/app.js
+++ server/app.js
@@ -32,11 +32,7 @@ var options = {
 // *** mongoose *** //
 //mongoose.connect('mongodb://config.database.username:config.database.password@18.221.155.123:27017/passport-social-auth',  { useMongoClient: true });
 //mongoose.connect('mongodb://127.0.0.1:27017/passport-social-auth');
-mongoose.connect('mongodb://18.221.155.123:27017/passport-social-auth', options, function(error) {
-  // Check error in initial connection. There is no 2nd param to the callback.
-  console.log("Can't connect");
-  throw error;
-});
+mongoose.connect('mongodb://18.221.155.123:27017/passport-social-auth', options);
 var db = mongoose.connection;
 
 // When successfully connected
